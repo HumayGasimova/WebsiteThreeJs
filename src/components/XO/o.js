7c98f3ead1cadff3bb579aa2045bfe81878f410f
Humay <humay.gasimova@recordunion.com>
2019-04-29 16:04:41 +0200

    Added animation on circle

--- src/components/XO/o.js
+++ src/components/XO/o.js
@@ -7,9 +7,9 @@ class O extends Component {
 
     render(){
         return(
-            <div className="o">
-                <div className="circle"/>
-            </div>
+            <svg width="150" height="160" xmlns="http://www.w3.org/2000/svg">
+                <circle class="circle" r="35" cy="50" cx="50" stroke-width="10" stroke="#F1EBD5" fill="none"/>
+            </svg>
         );
     }
 }
