04abf8ee4959c3ea3f07727a9e817defcb93eb59
Humay <crypto347@gmail.com>
2019-07-31 20:41:38 +0400

    closeLoginForm added

--- src/components/WelcomePage/Login/login.js
+++ src/components/WelcomePage/Login/login.js
@@ -32,44 +32,62 @@ class Login extends Component {
 
     constructor (props){
         super(props);
+        this.state={
+            showLoginForm: true
+        }
     }
     
     /**
     * Markup
     */
 
+   closeLoginForm = () => {
+       this.setState({
+           showLoginForm: false
+       })
+   }
+
+   renderLoginForm = () => {
+       if(this.state.showLoginForm === true){
+           return(
+               <div className="login">
+                    <div className="login-child">
+                        <div className="close-button" onClick={this.closeLoginForm}>X</div>
+                        <div className="login-text">USER AUTHENTICATION</div>
+                        <EmptyDivV1/>
+                        <InputField
+                            placeholder={" Email"}
+                            type={"text"}
+                            width={"200px"}
+                            height={"37px"}
+                            borderRadius={"7px"}
+                            />
+                        <EmptyDivV1/>   
+                        <InputField
+                            placeholder={" Password"}
+                            type={"password"}
+                            width={"200px"}
+                            height={"37px"}
+                            borderRadius={"7px"}
+                            />  
+                        <EmptyDivV1/>
+                        <Button
+                            type={"button"}
+                            disabled={false}
+                            text={"Login"}
+                            width={"200px"}
+                            height={"37px"}
+                            borderRadius={"7px"}
+                            />    
+                    </div>
+                </div>
+           )
+       }
+   }
     render(){
         return(
-            <div className="login">
-                <div className="login-child">
-                    <div className="close-button">X</div>
-                    <div className="login-text">USER AUTHENTICATION</div>
-                    <EmptyDivV1/>
-                    <InputField
-                        placeholder={" Email"}
-                        type={"text"}
-                        width={"200px"}
-                        height={"37px"}
-                        borderRadius={"7px"}
-                        />
-                    <EmptyDivV1/>   
-                    <InputField
-                        placeholder={" Password"}
-                        type={"password"}
-                        width={"200px"}
-                        height={"37px"}
-                        borderRadius={"7px"}
-                        />  
-                    <EmptyDivV1/>
-                    <Button
-                        type={"button"}
-                        disabled={false}
-                        text={"Login"}
-                        width={"200px"}
-                        height={"37px"}
-                        borderRadius={"7px"}
-                        />    
-                </div>
+            <div>
+                {this.renderLoginForm()}
             </div>
         );
     }
