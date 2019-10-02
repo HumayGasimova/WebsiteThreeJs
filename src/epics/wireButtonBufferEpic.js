028ef0bcd94f04aa1666bdb385ff02710bec5310
Humay <crypto347@gmail.com>
2019-10-02 17:46:02 +0400

    bufferWhen added

--- src/epics/wireButtonBufferEpic.js
+++ src/epics/wireButtonBufferEpic.js
@@ -1,18 +1,21 @@
-import { Observable, Rx } from 'rxjs';
+import { Observable, Rx, pipe } from 'rxjs';
 import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/bufferCount';
+import 'rxjs/add/operator/bufferWhen';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
 import 'rxjs/add/observable/of';
-import { bufferCount } from 'rxjs/operators';
+import 'rxjs/add/observable/interval';
 // import 'rxjs/add/operator/repeat';
 // .timeInterval(2000)
 import * as actionTypes from '../constants/actionTypes';
 import * as Actions from '../actions';
+import { delay } from 'rxjs/operators';
 
 function wireButtonBufferEpic(action$) {
     return action$
         .ofType(actionTypes.CLICK_WIRE_BUTTON)
-        .bufferCount(3)
+        .bufferWhen(()=> Observable.interval(700))
+        .filter(events => events.length >= 3)
         .mergeMap(action => {
             console.log("HEY")
             return Observable.of(
