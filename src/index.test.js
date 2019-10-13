ac11ef8e2dfd631ec8e1e2d32df3b5cb08442050
Humay <crypto347@gmail.com>
2019-10-13 10:29:59 +0400

    Jest fixed

--- src/index.test.js
+++
@@ -1,6 +0,0 @@
-// import sensitiveWords from '.';
-// import { TestScheduler } from 'rxjs-compat';
-
-// test('says hello world', () => (
-//     expect(sensitiveWords()).toBe('hello world')
-// ))
