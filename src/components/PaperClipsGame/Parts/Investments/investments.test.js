db07ceff661ebc85ac9dcd29134b4e164af643c5
Humay <crypto347@gmail.com>
2019-10-13 12:02:46 +0400

    Jest Enzyme works

--- src/components/PaperClipsGame/Parts/Investments/investments.test.js
+++ src/components/PaperClipsGame/Parts/Investments/investments.test.js
@@ -4,15 +4,20 @@ import { shallow } from 'enzyme';
 import { Investments } from './investments';
 import Button from '../../../../library/Button/button';
 
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('<Investments / >', () => {
     let wrapper;
 
     beforeEach(() => {
-        wrapper = shallow(<Investments />);
+        wrapper = shallow(<Investments investmentsCash={1000} investmentsStocks={1000} investmentsTotal={1000} investmentsLines={['']}/>);
     });
 
     it("should render three <Buttons />", () => {
+        // wrapper.setProps({})
        expect(wrapper.find(Button)).toHaveLength(3);
     })
 });
