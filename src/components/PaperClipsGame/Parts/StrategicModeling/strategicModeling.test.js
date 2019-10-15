df64f77c970eb33c807bf89b322ae97160b5a8a9
Humay <crypto347@gmail.com>
2019-10-15 17:05:16 +0400

    StrategicModeling component test added(Jest,Enzyme)

---
+++ src/components/PaperClipsGame/Parts/StrategicModeling/strategicModeling.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { StrategicModeling } from './strategicModeling';
+import Button from '../../../../library/Button/button';
+import Dropdown from '../../SmallParts/Dropdown/dropdown';
+
+describe('<StrategicModeling/>', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<StrategicModeling chosenListDropdown={['']}/>);
+    });
+
+    it("should render two <Buttons/> and one <Dropdown/>", () => {
+        // wrapper.setProps({})
+       expect(wrapper.find(Button)).toHaveLength(2);
+       expect(wrapper.find(Dropdown)).toHaveLength(1);
+    })
+});
