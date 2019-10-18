db6968495776b29bc980e49368eb2af75142e69d
Humay <crypto347@gmail.com>
2019-10-18 13:51:00 +0400

    reducer test updated(LOWER_PRICE)

--- src/reducers/businessReducer.test.js
+++ src/reducers/businessReducer.test.js
@@ -47,7 +47,7 @@ describe('businessReducer', () => {
         expect(reducer(initialState, action)).toEqual(state);
     })
 
-    it("should subtrack one from unsoldInventory", () => {
+    it("should subtract one from unsoldInventory", () => {
         const action = { 
             type: actionTypes.UPDATE_UNSOLD_INVENTORY
         }
@@ -57,7 +57,22 @@ describe('businessReducer', () => {
         }
         const state = {
             ...initialState, 
-            unsoldInventory: 5
+            unsoldInventory: 6
+        }
+        expect(reducer(initState, action)).toEqual(state);
+    })
+
+    it("should subtract 0.01 from paperclipPrice", () => {
+        const action = { 
+            type: actionTypes.LOWER_PRICE
+        }
+        const initState = {
+            ...initialState, 
+            paperclipPrice: 7.01
+        }
+        const state = {
+            ...initialState, 
+            paperclipPrice: 7
         }
         expect(reducer(initState, action)).toEqual(state);
     })
