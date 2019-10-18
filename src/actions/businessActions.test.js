cfbc0368e121cafbb8409826481b5e488e5576e7
Humay <crypto347@gmail.com>
2019-10-18 12:24:26 +0400

    ActionsTest(Jest,Enzyme) added

---
+++ src/actions/businessActions.test.js
@@ -0,0 +1,24 @@
+import configureStore from 'redux-mock-store';
+import * as Actions from '../actions';
+import * as actionTypes from "../constants/actionTypes";
+import { iterator } from 'rxjs/internal-compatibility';
+
+const mockStore = configureStore();
+const store = mockStore();
+
+describe('businessActions', () => {
+    beforeEach(() => { // Runs before each test in the suite
+      store.clearActions();
+    });
+  
+    it('Dispatches the correct action and payload', () => {
+        const expectedActions = [
+            {
+                type: actionTypes.MAKE_PAPERCLIP
+            }
+        ];
+
+        store.dispatch(Actions.makePaperclip());
+        expect(store.getActions()).toEqual(expectedActions);
+    })
+  });
