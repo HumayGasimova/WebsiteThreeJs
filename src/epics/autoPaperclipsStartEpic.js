58f640f4ea362ca9207ef52a3e993279506b5e68
Humay <crypto347@gmail.com>
2019-10-20 21:37:34 +0400

    redux Observable cleaning code

--- src/epics/autoPaperclipsStartEpic.js
+++ src/epics/autoPaperclipsStartEpic.js
@@ -1,19 +1,36 @@
-import { Observable } from 'rxjs';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/mapTo';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/repeat';
+import { Observable, interval } from 'rxjs';
+import { mergeMap, takeUntil } from 'rxjs/operators';
+import { ofType } from 'redux-observable';
+// import 'rxjs/add/operator/mergeMap';
+// import 'rxjs/add/operator/switchMap';
+// import 'rxjs/add/operator/mapTo';
+// import 'rxjs/add/observable/of';
+// import 'rxjs/add/operator/repeat';
 // .timeInterval(2000)
-import { interval } from "rxjs"
 import * as actionTypes from '../constants/actionTypes';
 import * as Actions from '../actions';
-import { mergeMap, takeUntil, ofType, repeat } from 'rxjs/operators';
+
 
 
 export const autoPaperclipsStartsEpic = (action$, state$) => 
-    action$
-    .ofType(actionTypes.AUTO_PAPERCLIPS_START)
+    action$.pipe(
+        ofType(actionTypes.AUTO_PAPERCLIPS_START),
+        mergeMap(action => {
+            return interval(state$.value.business.delayAutoPaperClippers).pipe(
+                mergeMap(() => Observable.of(
+                            Actions.makePaperclip()
+                        )   
+                        // .delay(1000)
+                        // .repeat(state$.value.business.autoClippersPerSec)
+                ),
+                takeUntil(action$.ofType(actionTypes.STOP))
+            )
+       })
+    )
+    
+
+
+   
     // .mergeMap(action => {
     //     if(state$.value.business.autoClippersPerSec > 60){
     //         return interval(1000).pipe(
@@ -38,17 +55,7 @@ export const autoPaperclipsStartsEpic = (action$, state$) =>
     //         )
     //     }
     //  })
-        .mergeMap(action => {
-            return interval(state$.value.business.delayAutoPaperClippers).pipe(
-                mergeMap(() => Observable.of(
-                            Actions.makePaperclip()
-                        )   
-                        // .delay(1000)
-                        // .repeat(state$.value.business.autoClippersPerSec)
-                ),
-                takeUntil(action$.ofType(actionTypes.STOP))
-            )
-       })
+     
 
 
    
