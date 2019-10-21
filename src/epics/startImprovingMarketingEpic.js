135ee87b84f85d19ff781690accccefc0e3abbdd
Humay <crypto347@gmail.com>
2019-10-21 13:47:55 +0400

    fixing epic startImprovingMarketingEpic

--- src/epics/startImprovingMarketingEpic.js
+++ src/epics/startImprovingMarketingEpic.js
@@ -1,23 +1,25 @@
 import { Observable } from 'rxjs';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/delay';
+import { mergeMap } from 'rxjs/operators';
+import { ofType } from 'redux-observable';
+// import { Observable } from 'rxjs';
+// import 'rxjs/add/operator/mergeMap';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/observable/of';
+// import 'rxjs/add/operator/delay';
 import * as actionTypes from '../constants/actionTypes';
 import * as Actions from '../actions';
 
-function startImprovingMarketingEpic(action$) { 
-    return action$
-        .ofType(actionTypes.START_IMPROVING_MARKETING)
-        .mergeMap(action => {
+export const startImprovingMarketingEpic = (action$, state$) => 
+    action$.pipe(
+        ofType(actionTypes.START_IMPROVING_MARKETING),
+        mergeMap(action => {
             return Observable.of(
                 Actions.improveMarketing(action.act),
                 Actions.calcDelayUnsoldInventary(),
                 Actions.stopUpdatingUnsoldInventory(),
-                Actions.startUpdatingUnsoldInventory(),
-
+                Actions.startUpdatingUnsoldInventory()
             )
-        })       
-}
+        }) 
+    )
 
 export default startImprovingMarketingEpic;
