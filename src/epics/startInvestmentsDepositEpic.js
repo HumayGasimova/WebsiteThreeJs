c1ccc0b44982aeaf1fff6f207cd59b50714b41ac
Humay <crypto347@gmail.com>
2019-10-21 13:59:07 +0400

    fixing epic startInvestmentsDepositEpic

--- src/epics/startInvestmentsDepositEpic.js
+++ src/epics/startInvestmentsDepositEpic.js
@@ -1,15 +1,18 @@
 import { Observable } from 'rxjs';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/delay';
+import { mergeMap } from 'rxjs/operators';
+import { ofType } from 'redux-observable';
+// import { Observable } from 'rxjs';
+// import 'rxjs/add/operator/mergeMap';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/observable/of';
+// import 'rxjs/add/operator/delay';
 import * as actionTypes from '../constants/actionTypes';
 import * as Actions from '../actions';
 
-function startInvestmentsDepositEpic(action$, state$) { 
-    return action$
-        .ofType(actionTypes.START_INVESTMENTS_DEPOSIT)
-        .mergeMap(action => {
+export const startInvestmentsDepositEpic = (action$, state$) =>
+    action$.pipe(
+        ofType(actionTypes.START_INVESTMENTS_DEPOSIT),
+        mergeMap(action => {
             let deposit = state$.value.business.funds
             return Observable.of(
                 Actions.getDeposit(),
@@ -18,7 +21,7 @@ function startInvestmentsDepositEpic(action$, state$) {
                 Actions.startCountingRisk(),
                 Actions.startApplyingProfitLoss()
             )
-        })       
-}
-
+        }) 
+    )
+        
 export default startInvestmentsDepositEpic;
