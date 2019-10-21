f560b3a53e920027b820c532e75dbf6765204b97
Humay <crypto347@gmail.com>
2019-10-21 12:30:27 +0400

    fixing epics decreaseOpsEpic

--- src/epics/decreaseOpsEpic.js
+++ src/epics/decreaseOpsEpic.js
@@ -1,25 +1,32 @@
 import { Observable } from 'rxjs';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/take';
-import 'rxjs/add/operator/delay';
+import { switchMap, delay, repeat } from 'rxjs/operators';
+import { ofType } from 'redux-observable';
+// import { Observable } from 'rxjs';
+// import 'rxjs/add/operator/mergeMap';
+// import 'rxjs/add/operator/switchMap';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/take';
+// import 'rxjs/add/operator/delay';
 import * as actionTypes from '../constants/actionTypes';
 import * as Actions from '../actions';
-import { mergeMap, takeUntil, ofType, repeat } from 'rxjs/operators';
-import { interval } from "rxjs"
+// import { mergeMap, takeUntil, ofType, repeat } from 'rxjs/operators';
+// import { interval } from "rxjs"
 
-function decreaseOpsEpic(action$, state$) { 
-        return action$
-            .ofType(actionTypes.START_DECREASING_OPS)
-            .switchMap(action => {
-                return Observable.of(
-                            Actions.decreaseOps()
-                        )
-                    .delay(10)
-                    .repeat(state$.value.business.ops-state$.value.business.opsMax)               
-            })
-    }
+export const decreaseOpsEpic = (action$, state$) => 
+    action$.pipe(
+        ofType(actionTypes.START_DECREASING_OPS),
+        switchMap(action => {
+            return Observable.of(
+                        Actions.decreaseOps()
+                    ).pipe(
+                        delay(10),
+                        repeat(state$.value.business.ops-state$.value.business.opsMax)  
+                    )
+                             
+        })
+    )
+           
+    
 
 // function decreaseOpsEpic(action$, state$) { 
 //     return action$
