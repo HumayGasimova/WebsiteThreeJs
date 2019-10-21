b6053665f76044b097ded5a978e12683345350b9
Humay <crypto347@gmail.com>
2019-10-21 12:07:15 +0400

    fixing epics checkWireEpic

--- src/epics/checkWireEpic.js
+++ src/epics/checkWireEpic.js
@@ -1,17 +1,20 @@
-import { Observable, Rx } from 'rxjs';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/of';
+import { Observable } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+import { ofType } from 'redux-observable';
+// import { Observable, Rx } from 'rxjs';
+// import 'rxjs/add/operator/mergeMap';
+// import 'rxjs/add/operator/switchMap';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/observable/of';
 // import 'rxjs/add/operator/repeat';
 // .timeInterval(2000)
 import * as actionTypes from '../constants/actionTypes';
 import * as Actions from '../actions';
 
-function checkWireEpic(action$, state$) {
-    return action$
-        .ofType(actionTypes.CHECK_EXISTENCE_OF_WIRE)
-        .mergeMap(action => {
+export const checkWireEpic = (action$, state$) => 
+    action$.pipe(
+        ofType(actionTypes.CHECK_EXISTENCE_OF_WIRE),
+        mergeMap(action => {
             if(state$.value.business.wire === 0 ){
                 return Observable.of(
                     Actions.stop(),
@@ -26,7 +29,6 @@ function checkWireEpic(action$, state$) {
             }
                   
         })
-        
-}
-
+    )
+    
 export default checkWireEpic;
