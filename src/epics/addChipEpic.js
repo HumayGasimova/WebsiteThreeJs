735e0609e2a2eb2ee633970d2be1ed1008d44816
Humay <crypto347@gmail.com>
2019-10-21 09:53:27 +0400

    fixing epics + autoClippersIsShown logic updated

--- src/epics/addChipEpic.js
+++ src/epics/addChipEpic.js
@@ -1,19 +1,22 @@
-import { Observable } from 'rxjs';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/delay';
+import { Observable} from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+import { ofType } from 'redux-observable';
+// import { Observable } from 'rxjs';
+// import 'rxjs/add/operator/mergeMap';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/observable/of';
+// import 'rxjs/add/operator/delay';
 import * as actionTypes from '../constants/actionTypes';
 import * as Actions from '../actions';
 
-function addChipEpic(action$, state$) {
-    return action$
-        .ofType(actionTypes.ADD_CHIP)
-        .mergeMap(action => {
+export const addChipEpic = (action$, state$) => 
+    action$.pipe(
+        ofType(actionTypes.ADD_CHIP),
+        mergeMap(action => {
            return Observable.of(
                 Actions.toggleChip(true, action.obj.chipsNumber)
            ) 
-        })       
-}
-
+        })    
+    )
+         
 export default addChipEpic;
--- src/epics/autoPaperClippersAddOneEpic.js
+++ src/epics/autoPaperClippersAddOneEpic.js
@@ -1,22 +1,26 @@
-import { Observable, Rx } from 'rxjs';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/of';
+import { Observable, interval } from 'rxjs';
+import { mergeMap, takeUntil } from 'rxjs/operators';
+import { ofType } from 'redux-observable';
+
+// import { Observable, Rx } from 'rxjs';
+// import 'rxjs/add/operator/mergeMap';
+// import 'rxjs/add/operator/switchMap';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/observable/of';
 // import 'rxjs/add/operator/repeat';
 // .timeInterval(2000)
 import * as actionTypes from '../constants/actionTypes';
 import * as Actions from '../actions';
 
-function autoPaperClippersAddOneEpic(action$, state$) {
-    return action$
-        .ofType(actionTypes.AUTO_PAPERCLIPS)        
-        .mergeMap(action => {
+export const autoPaperClippersAddOneEpic = (action$, state$) => 
+    action$.pipe(
+        ofType(actionTypes.AUTO_PAPERCLIPS) ,      
+        mergeMap(action => {
             return Observable.of(
                 Actions.autoClippersAddOne(),
                 Actions.autoPaperclipsStart()
             )    
         }) 
-}
-
+    )
+       
 export default autoPaperClippersAddOneEpic;
