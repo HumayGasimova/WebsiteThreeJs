0e2079dc6aa3d8f30868b989bcfe75f807d3c2ae
Humay <crypto347@gmail.com>
2019-10-21 16:16:25 +0400

    fixing epic throwTheHadwingerProblemEpic

--- src/epicsProject/throwTheHadwingerProblemEpic.js
+++ src/epicsProject/throwTheHadwingerProblemEpic.js
@@ -1,21 +1,24 @@
 import { Observable } from 'rxjs';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/take';
+import { mergeMap, take } from 'rxjs/operators';
+import { ofType } from 'redux-observable';
+// import { Observable } from 'rxjs';
+// import 'rxjs/add/operator/mergeMap';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/observable/of';
+// import 'rxjs/add/operator/take';
 import * as actionTypes from '../constants/actionTypes';
 import * as Actions from '../actions';
 import * as projectsToAdd from '../constants/projectsToAdd';
 
-function throwTheHadwingerProblemEpic(action$) { 
-    return action$
-        .ofType(actionTypes.ADD_THE_HADWINGER_PROBLEM)
-        .take(1)
-        .mergeMap(action => {
-            return Observable.of(
-              Actions.addProject(projectsToAdd.TheHadwigerProblem)
-            ) 
-        })
-}
+export const throwTheHadwingerProblemEpic = (action$, state$) => 
+  action$.pipe(
+    ofType(actionTypes.ADD_THE_HADWINGER_PROBLEM),
+    take(1),
+    mergeMap(action => {
+        return Observable.of(
+          Actions.addProject(projectsToAdd.TheHadwigerProblem)
+        ) 
+    })
+  )
 
 export default throwTheHadwingerProblemEpic;
