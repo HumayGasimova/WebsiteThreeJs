dc72022848295e45cd1d24f87d57ca3ff842b3b4
Humay <crypto347@gmail.com>
2019-10-21 14:40:10 +0400

    fiixing epic startUpdatingScreenEpic

--- src/epics/startUpdatingScreenEpic.js
+++ src/epics/startUpdatingScreenEpic.js
@@ -1,14 +1,17 @@
-import { Observable } from 'rxjs';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/mapTo';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/repeat';
+import { Observable, interval } from 'rxjs';
+import { mergeMap, takeUntil } from 'rxjs/operators';
+import { ofType } from 'redux-observable';
+// import { Observable } from 'rxjs';
+// import 'rxjs/add/operator/mergeMap';
+// import 'rxjs/add/operator/switchMap';
+// import 'rxjs/add/operator/mapTo';
+// import 'rxjs/add/observable/of';
+// import 'rxjs/add/operator/repeat';
 // .timeInterval(2000)
-import { interval } from "rxjs"
+// import { interval } from "rxjs"
 import * as actionTypes from '../constants/actionTypes';
 import * as Actions from '../actions';
-import { mergeMap, takeUntil, ofType, repeat } from 'rxjs/operators';
+// import { mergeMap, takeUntil, ofType, repeat } from 'rxjs/operators';
 /**
 * Utility
 */
@@ -16,32 +19,33 @@ import { mergeMap, takeUntil, ofType, repeat } from 'rxjs/operators';
 import * as Utility from '../utility';
 
 export const startUpdatingScreenEpic = (action$, state$) => 
-    action$
-    .ofType(actionTypes.START_UPDATING_SCREEN)
-    .mergeMap(action => {
-        return interval(state$.value.business.delayUpdatingInvScreen).pipe(
-            mergeMap(() => {
-                let randomNum = Utility.getRandomAmount(state$.value.business.fakeInvestmentsCash);
-                let randomPrice = Utility.getRandomPrice();
-                let randomProfitLoss = Utility.getRandomProfitLoss();
-                let randomLetters = Utility.getRandomLetters();
-                let obj = {
-                    stock: randomLetters,
-                    amt: randomNum,
-                    price: randomPrice,
-                    total: randomNum * randomPrice,
-                    profitLoss: randomNum * randomProfitLoss
+    action$.pipe(
+        ofType(actionTypes.START_UPDATING_SCREEN),
+        mergeMap(action => {
+            return interval(state$.value.business.delayUpdatingInvScreen).pipe(
+                mergeMap(() => {
+                    let randomNum = Utility.getRandomAmount(state$.value.business.fakeInvestmentsCash);
+                    let randomPrice = Utility.getRandomPrice();
+                    let randomProfitLoss = Utility.getRandomProfitLoss();
+                    let randomLetters = Utility.getRandomLetters();
+                    let obj = {
+                        stock: randomLetters,
+                        amt: randomNum,
+                        price: randomPrice,
+                        total: randomNum * randomPrice,
+                        profitLoss: randomNum * randomProfitLoss
+                    }
+                    
+                    return Observable.of(
+                        Actions.addInvestmentsLine(obj, true)
+                    )   
                 }
-                
-                return Observable.of(
-                    Actions.addInvestmentsLine(obj, true)
-                )   
-            }
-                    // .delay(1000)
-                    // .repeat(state$.value.business.autoClippersPerSec)
-            ),
-            takeUntil(action$.ofType(actionTypes.STOP_UPDATING_SCREEN))
-        )
-    })
-
+                        // .delay(1000)
+                        // .repeat(state$.value.business.autoClippersPerSec)
+                ),
+                takeUntil(action$.ofType(actionTypes.STOP_UPDATING_SCREEN))
+            )
+        })
+    )
+   
 export default startUpdatingScreenEpic;
