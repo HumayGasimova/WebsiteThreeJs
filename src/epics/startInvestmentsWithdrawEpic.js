a950e6931ff7d514f97eeba6923b12b3539b4e16
Humay <crypto347@gmail.com>
2019-10-21 14:01:18 +0400

    fixing epic startInvestmentsWithdrawEpic

--- src/epics/startInvestmentsWithdrawEpic.js
+++ src/epics/startInvestmentsWithdrawEpic.js
@@ -1,15 +1,18 @@
 import { Observable } from 'rxjs';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/delay';
+import { mergeMap } from 'rxjs/operators';
+import { ofType } from 'redux-observable';
+// import { Observable } from 'rxjs';
+// import 'rxjs/add/operator/mergeMap';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/observable/of';
+// import 'rxjs/add/operator/delay';
 import * as actionTypes from '../constants/actionTypes';
 import * as Actions from '../actions';
 
-function startInvestmentsWithdrawEpic(action$, state$) { 
-    return action$
-        .ofType(actionTypes.START_INVESTMENTS_WITHDRAW)
-        .mergeMap(action => {
+export const startInvestmentsWithdrawEpic = (action$, state$) =>
+    action$.pipe(
+        ofType(actionTypes.START_INVESTMENTS_WITHDRAW),
+        mergeMap(action => {
             let withdraw = state$.value.business.investmentsCash;
             let total;
             if(state$.value.business.investmentsTotal === 0){
@@ -25,7 +28,7 @@ function startInvestmentsWithdrawEpic(action$, state$) {
                 Actions.updateFakeInvestmentsCash(total),
                 Actions.updateFundsWithdraw(withdraw)
             )
-        })       
-}
+        })   
+    )
 
 export default startInvestmentsWithdrawEpic;
