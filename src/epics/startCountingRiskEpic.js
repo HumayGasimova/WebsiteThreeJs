ff0e89594d8af7c30ea322e7e0740ecac1afc628
Humay <crypto347@gmail.com>
2019-10-21 13:11:39 +0400

    fixing epics startCountingRiskEpic

--- src/epics/startCountingRiskEpic.js
+++ src/epics/startCountingRiskEpic.js
@@ -1,14 +1,17 @@
-import { Observable } from 'rxjs';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/mapTo';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/repeat';
+import { Observable, interval } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+import { ofType } from 'redux-observable';
+// import { Observable } from 'rxjs';
+// import 'rxjs/add/operator/mergeMap';
+// import 'rxjs/add/operator/switchMap';
+// import 'rxjs/add/operator/mapTo';
+// import 'rxjs/add/observable/of';
+// import 'rxjs/add/operator/repeat';
 // .timeInterval(2000)
-import { interval } from "rxjs"
+// import { interval } from "rxjs"
 import * as actionTypes from '../constants/actionTypes';
 import * as Actions from '../actions';
-import { mergeMap, takeUntil, ofType, repeat } from 'rxjs/operators';
+// import { mergeMap, takeUntil, ofType, repeat } from 'rxjs/operators';
 
 /**
 * Utility
@@ -17,45 +20,46 @@ import { mergeMap, takeUntil, ofType, repeat } from 'rxjs/operators';
 import * as Utility from '../utility';
 
 export const startCountingRiskEpic = (action$, state$) => 
-    action$
-    .ofType(actionTypes.START_COUNTING_RISK)
-    .mergeMap(action => {
-        return interval(9000).pipe(
-            mergeMap(() => {
-                let depositCash = +state$.value.business.fakeInvestmentsCash;
-                let half1 = Utility.getRandomPercent();
-                let half2 = 100 - half1;
-                let cash = +(depositCash * half1 /100).toFixed();
-                let stocks = +(depositCash * half2 /100).toFixed();
-                let total = cash + stocks;
-            
-            
-
-            //    switch(state$.value.business.risk){
-            //        case "lowRisk":
-            //             if(half1 > half2){
-            //                 cash = 
-            //             }
-            //            return
-            //            break;
-            //         case "medRisk":
-            //            return
-            //            break;
-            //         case "highRisk":
-            //             return
-            //             break;
-            //    }
-                return Observable.of(
-                    Actions.updateInvestmentsTotal(total),
-                    Actions.updateInvestmentsCash(cash),
-                    Actions.updateInvestmentsStocks(stocks),
-                )   
-            }
-                    // .delay(1000)
-                    // .repeat(state$.value.business.autoClippersPerSec)
-            ),
-            // takeUntil(action$.ofType(actionTypes.STOP))
-        )
-})
-
+    action$.pipe(
+        ofType(actionTypes.START_COUNTING_RISK),
+        mergeMap(action => {
+            return interval(9000).pipe(
+                mergeMap(() => {
+                    let depositCash = +state$.value.business.fakeInvestmentsCash;
+                    let half1 = Utility.getRandomPercent();
+                    let half2 = 100 - half1;
+                    let cash = +(depositCash * half1 /100).toFixed();
+                    let stocks = +(depositCash * half2 /100).toFixed();
+                    let total = cash + stocks;
+                
+                
+    
+                //    switch(state$.value.business.risk){
+                //        case "lowRisk":
+                //             if(half1 > half2){
+                //                 cash = 
+                //             }
+                //            return
+                //            break;
+                //         case "medRisk":
+                //            return
+                //            break;
+                //         case "highRisk":
+                //             return
+                //             break;
+                //    }
+                    return Observable.of(
+                        Actions.updateInvestmentsTotal(total),
+                        Actions.updateInvestmentsCash(cash),
+                        Actions.updateInvestmentsStocks(stocks),
+                    )   
+                }
+                        // .delay(1000)
+                        // .repeat(state$.value.business.autoClippersPerSec)
+                ),
+                // takeUntil(action$.ofType(actionTypes.STOP))
+            )
+        })
+    )
+    
 export default startCountingRiskEpic;
