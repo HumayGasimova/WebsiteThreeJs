4f6d097bb7966ad3dbf316c0dcbea9811d4f3628
Humay <crypto347@gmail.com>
2019-10-21 12:50:52 +0400

    fixing epics raisePriceEpic

--- src/epics/raisePriceEpic.js
+++ src/epics/raisePriceEpic.js
@@ -1,22 +1,25 @@
 import { Observable } from 'rxjs';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/delay';
+import { mergeMap } from 'rxjs/operators';
+import { ofType } from 'redux-observable';
+// import { Observable } from 'rxjs';
+// import 'rxjs/add/operator/mergeMap';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/observable/of';
+// import 'rxjs/add/operator/delay';
 import * as actionTypes from '../constants/actionTypes';
 import * as Actions from '../actions';
 
-function raisePriceEpic(action$) { 
-    return action$
-        .ofType(actionTypes.RAISE_PRICE, actionTypes.LOWER_PRICE)
-        .mergeMap(action => {
+export const raisePriceEpic = (action$, state$) => 
+    action$.pipe(
+        ofType(actionTypes.RAISE_PRICE, actionTypes.LOWER_PRICE),
+        mergeMap(action => {
             return Observable.of(
                 Actions.updatePublicDemand(),
                 Actions.calcDelayUnsoldInventary(),
                 Actions.stopUpdatingUnsoldInventory(),
                 Actions.startUpdatingUnsoldInventory(),
             )
-        })       
-}
+        })     
+    )
 
 export default raisePriceEpic;
