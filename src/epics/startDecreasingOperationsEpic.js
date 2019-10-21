00225e619e06545aa8f7d3e256c3f9f798c6cc41
Humay <crypto347@gmail.com>
2019-10-21 13:42:23 +0400

    fixing epics startDecreasingOperationsEpic

--- src/epics/startDecreasingOperationsEpic.js
+++ src/epics/startDecreasingOperationsEpic.js
@@ -1,25 +1,28 @@
 import { Observable } from 'rxjs';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/delay';
+import { switchMap, debounceTime } from 'rxjs/operators';
+import { ofType } from 'redux-observable';
+// import { Observable } from 'rxjs';
+// import 'rxjs/add/operator/mergeMap';
+// import 'rxjs/add/operator/switchMap';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/debounceTime';
+// import 'rxjs/add/operator/delay';
 import * as actionTypes from '../constants/actionTypes';
 import * as Actions from '../actions';
-import { mergeMap, takeUntil, ofType, repeat } from 'rxjs/operators';
-import { interval } from "rxjs"
+// import { mergeMap, takeUntil, ofType, repeat } from 'rxjs/operators';
+// import { interval } from "rxjs"
 
-function startDecreasingOperationsEpic(action$, state$) { 
-        return action$
-            .ofType(actionTypes.START_DECREASING_OPERATIONS)
-            .switchMap(action => {
-                return Observable.of(
-                            Actions.startDecreasingOps(),
-                            // Actions.addCaptureVal(-action.captureNumber),
-                        )
-            })
-            // .delay(15000)
-            .debounceTime(900)
-    }
-
+export const startDecreasingOperationsEpic = (action$, state$) => 
+    action$.pipe(
+        ofType(actionTypes.START_DECREASING_OPERATIONS),
+        switchMap(action => {
+            return Observable.of(
+                        Actions.startDecreasingOps(),
+                        // Actions.addCaptureVal(-action.captureNumber),
+                    )
+        }),
+        // .delay(15000)
+        debounceTime(900)
+    )
+            
 export default startDecreasingOperationsEpic;
