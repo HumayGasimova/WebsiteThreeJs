68cec1f7bb11be488c113e0e78ef24d8fefbd3e3
Humay <crypto347@gmail.com>
2019-10-21 14:07:48 +0400

    fixing epic startMegaClippersEpic

--- src/epics/startMegaClippersEpic.js
+++ src/epics/startMegaClippersEpic.js
@@ -1,20 +1,23 @@
-import { Observable } from 'rxjs';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/mapTo';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/repeat';
+import { Observable, interval } from 'rxjs';
+import { mergeMap, takeUntil } from 'rxjs/operators';
+import { ofType } from 'redux-observable';
+// import { Observable } from 'rxjs';
+// import 'rxjs/add/operator/mergeMap';
+// import 'rxjs/add/operator/switchMap';
+// import 'rxjs/add/operator/mapTo';
+// import 'rxjs/add/observable/of';
+// import 'rxjs/add/operator/repeat';
 // .timeInterval(2000)
-import { interval } from "rxjs"
+// import { interval } from "rxjs"
 import * as actionTypes from '../constants/actionTypes';
 import * as Actions from '../actions';
-import { mergeMap, takeUntil, ofType, repeat } from 'rxjs/operators';
+// import { mergeMap, takeUntil, ofType, repeat } from 'rxjs/operators';
 
 
 export const startMegaClippersEpic = (action$, state$) => 
-    action$
-    .ofType(actionTypes.START_MEGACLIPPERS)
-        .mergeMap(action => {
+    action$.pipe(
+        ofType(actionTypes.START_MEGACLIPPERS),
+        mergeMap(action => {
             return interval(1000).pipe(
                 mergeMap(() => {
                     let wire = state$.value.business.wire;
@@ -32,7 +35,6 @@ export const startMegaClippersEpic = (action$, state$) =>
                 takeUntil(action$.ofType(actionTypes.STOP))
             )
        })
-
-
-   
+    )
+    
 export default startMegaClippersEpic;
