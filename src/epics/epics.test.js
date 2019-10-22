3647320fd5187b6ccdeda93c4d74ca2251b603f1
Humay <crypto347@gmail.com>
2019-10-22 17:31:19 +0400

    epic testing in progress 2

--- src/epics/epics.test.js
+++ src/epics/epics.test.js
@@ -1,23 +1,25 @@
-import { of } from 'rxjs';
+import { ActionsObservable } from 'redux-observable';
 import { toArray } from 'rxjs/operators';
 import * as Epic from './index';
 import * as Actions from "../actions";
 import * as actionTypes from "../constants/actionTypes";
+import { doesNotReject } from 'assert';
 
 describe('addChipEpic', () => {
-    it('dispatches actions to toggleChip', () => {
-      const action$ = of(Actions.addChip({a:4,b:5}));
+    it('dispatches actions to toggleChip', (done) => {
+      const action$ = ActionsObservable.of(Actions.addChip({a:4,b:5}));
       const expectedOutputActions = [
             {type: "IS_SAVING"},
             {type: "SAVING_ERROR", error: 'save failed'},
       ];
-      Epic.addChipEpic(action$, {}).pipe(
-        toArray()
-      )
+
+      Epic.addChipEpic(action$, {})
+      .toArray()
       .subscribe((outputActions) => {
         console.log(outputActions)
         expect(outputActions).toEqual(expectedOutputActions);
       })
+      done();
        
     });
 });
