9c2a58a6ff0268fd4360f989cf1f915ba88b4467
Humay <crypto347@gmail.com>
2019-10-23 10:26:47 +0400

    await async(config in babelIrc added) + epic test addChipEpic added

--- src/epics/epics.test.js
+++ src/epics/epics.test.js
@@ -1,25 +1,49 @@
 import { ActionsObservable } from 'redux-observable';
 import { toArray } from 'rxjs/operators';
 import * as Epic from './index';
+import { addChipEpic }from './addChipEpic'
 import * as Actions from "../actions";
 import * as actionTypes from "../constants/actionTypes";
-import { doesNotReject } from 'assert';
 
 describe('addChipEpic', () => {
-    it('dispatches actions to toggleChip', (done) => {
-      const action$ = ActionsObservable.of(Actions.addChip({a:4,b:5}));
-      const expectedOutputActions = [
-            {type: "IS_SAVING"},
-            {type: "SAVING_ERROR", error: 'save failed'},
-      ];
 
-      Epic.addChipEpic(action$, {})
-      .toArray()
-      .subscribe((outputActions) => {
-        console.log(outputActions)
-        expect(outputActions).toEqual(expectedOutputActions);
-      })
-      done();
+  it('EPICs',
+    async () => {
+      const action$ = ActionsObservable.of({
+        type: actionTypes.ADD_CHIP,
+        obj: {
+          chipsNumber: 'chipX',
+          showChip: false,
+        }
+      });
+      const epic$ = addChipEpic(action$);
+      const array =[];
+      await epic$.subscribe(
+        (action) => array.push(action)
+      )
+  
+      expect(array).toEqual([
+        { type: actionTypes.TOGGLE_CHIP,
+          val: true,
+          chipsNumber: "chipX" }
+      ])
+    }
+  )
+  
+    // it('dispatches actions to toggleChip', (done) => {
+    //   const action$ = ActionsObservable.of(Actions.addChip({a:4,b:5}));
+    //   const expectedOutputActions = [
+    //         {type: "IS_SAVING"},
+    //         {type: "SAVING_ERROR", error: 'save failed'},
+    //   ];
+
+    //   Epic.addChipEpic(action$, {})
+    //   .toArray()
+    //   .subscribe((outputActions) => {
+    //     console.log(outputActions)
+    //     expect(outputActions).toEqual(expectedOutputActions);
+    //   })
+    //   done();
        
-    });
+    // });
 });
