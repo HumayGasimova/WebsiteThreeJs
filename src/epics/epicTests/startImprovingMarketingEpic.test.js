4edaf866c2a30391c5215ff8a2656d9cfc5b774d
Humay <crypto347@gmail.com>
2019-10-23 16:33:36 +0400

    jest startImprovingMarketingEpic added

---
+++ src/epics/epicTests/startImprovingMarketingEpic.test.js
@@ -0,0 +1,31 @@
+import { ActionsObservable } from 'redux-observable';
+import * as Epic from '../index'
+import * as Actions from "../../actions";
+import * as actionTypes from "../../constants/actionTypes";
+
+describe('startImprovingMarketingEpic', () => {
+
+  it('should return IMPROVE_MARKETING, CALC_DELAY_UNSOLD_INVENTARY, STOP_UPDATING_UNSOLD_INVENTORY, START_UPDATING_UNSOLD_INVENTORY',
+    () => {
+      const action$ = ActionsObservable.of({
+        type: actionTypes.START_IMPROVING_MARKETING,
+        act: 50
+      });
+      const epic$ = Epic.startImprovingMarketingEpic(action$);
+      const array = [];
+      epic$.subscribe(
+        (action) => array.push(action)
+      )
+
+      expect(array).toEqual([
+        { 
+            type: actionTypes.IMPROVE_MARKETING,
+            val: 50
+        },
+        { type: actionTypes.CALC_DELAY_UNSOLD_INVENTARY },
+        { type: actionTypes.STOP_UPDATING_UNSOLD_INVENTORY },
+        { type: actionTypes.START_UPDATING_UNSOLD_INVENTORY }
+      ])
+    }
+  )
+});
