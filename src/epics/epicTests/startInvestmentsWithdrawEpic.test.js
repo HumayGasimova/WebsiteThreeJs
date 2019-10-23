04c1e21078a377f05461b4394646ed2b1353fbf9
Humay <crypto347@gmail.com>
2019-10-23 17:16:27 +0400

    jest startInvestmentsWithdrawEpic added

---
+++ src/epics/epicTests/startInvestmentsWithdrawEpic.test.js
@@ -0,0 +1,87 @@
+import { ActionsObservable } from 'redux-observable';
+import * as Epic from '../index'
+import * as Actions from "../../actions";
+import * as actionTypes from "../../constants/actionTypes";
+
+describe('startInvestmentsWithdrawEpic', () => {
+
+  it('should (if investmentsTotal is equal to 0 (total = 0)) return UPDATE_INVESTMENTS_TOTAL, UPDATE_INVESTMENTS_CASH, UPDATE_FAKE_INVESTMENTS_CASH, UPDATE_FUNDS_WITHDRAW',
+    () => {
+      const action$ = ActionsObservable.of({
+        type: actionTypes.START_INVESTMENTS_WITHDRAW
+      });
+      const state$ = {
+        value: {
+            business: {
+                investmentsCash: 500,
+                investmentsTotal: 0 
+            }
+        }
+        }
+      const epic$ = Epic.startInvestmentsWithdrawEpic(action$, state$);
+      const array = [];
+      epic$.subscribe(
+        (action) => array.push(action)
+      )
+
+      expect(array).toEqual([
+        { 
+            type: actionTypes.UPDATE_INVESTMENTS_TOTAL,
+            total: 0
+        },
+        { 
+            type: actionTypes.UPDATE_INVESTMENTS_CASH,
+            cash: 0
+        },
+        { 
+            type: actionTypes.UPDATE_FAKE_INVESTMENTS_CASH,
+            cash: 0
+        },
+        { 
+            type: actionTypes.UPDATE_FUNDS_WITHDRAW,
+            val: 500
+        }
+      ])
+    }
+  )
+
+  it('should (if investmentsTotal is equal to 0 (total = investmentsTotal - investmentsCash)) return UPDATE_INVESTMENTS_TOTAL, UPDATE_INVESTMENTS_CASH, UPDATE_FAKE_INVESTMENTS_CASH, UPDATE_FUNDS_WITHDRAW',
+    () => {
+      const action$ = ActionsObservable.of({
+        type: actionTypes.START_INVESTMENTS_WITHDRAW
+      });
+      const state$ = {
+        value: {
+            business: {
+                investmentsCash: 500,
+                investmentsTotal: 700
+            }
+        }
+        }
+      const epic$ = Epic.startInvestmentsWithdrawEpic(action$, state$);
+      const array = [];
+      epic$.subscribe(
+        (action) => array.push(action)
+      )
+
+      expect(array).toEqual([
+        { 
+            type: actionTypes.UPDATE_INVESTMENTS_TOTAL,
+            total: 200
+        },
+        { 
+            type: actionTypes.UPDATE_INVESTMENTS_CASH,
+            cash: 0
+        },
+        { 
+            type: actionTypes.UPDATE_FAKE_INVESTMENTS_CASH,
+            cash: 200
+        },
+        { 
+            type: actionTypes.UPDATE_FUNDS_WITHDRAW,
+            val: 500
+        }
+      ])
+    }
+  )
+});
