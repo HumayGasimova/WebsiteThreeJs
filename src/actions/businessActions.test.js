608efc737188110bc3d7760dc291cfac39547b4d
Humay <crypto347@gmail.com>
2019-10-25 18:02:44 +0400

    jest addMegaClippers added

--- src/actions/businessActions.test.js
+++ src/actions/businessActions.test.js
@@ -1051,4 +1051,16 @@ describe('businessActions', () => {
         expect(store.getActions()).toEqual(expectedActions);
         expect(store.getActions()).toMatchSnapshot();
     })
+
+    it('Dispatches the correct action and payload (addMegaClippers)', () => {
+        const expectedActions = [
+            {
+                type: actionTypes.ADD_MEGA_CLIPPERS
+            }
+        ];
+
+        store.dispatch(Actions.addMegaClippers());
+        expect(store.getActions()).toEqual(expectedActions);
+        expect(store.getActions()).toMatchSnapshot();
+    })
   });
