1f5a4636a086629aeeea9e6ecf75e146a952c2cc
Humay <crypto347@gmail.com>
2019-10-25 16:48:07 +0400

    jest decreaseOps added

--- src/actions/businessActions.test.js
+++ src/actions/businessActions.test.js
@@ -409,4 +409,16 @@ describe('businessActions', () => {
         expect(store.getActions()).toEqual(expectedActions);
         expect(store.getActions()).toMatchSnapshot();
     })
+
+    it('Dispatches the correct action and payload (decreaseOps)', () => {
+        const expectedActions = [
+            {
+                type: actionTypes.DECREASE_OPS
+            }
+        ];
+
+        store.dispatch(Actions.decreaseOps());
+        expect(store.getActions()).toEqual(expectedActions);
+        expect(store.getActions()).toMatchSnapshot();
+    })
   });
