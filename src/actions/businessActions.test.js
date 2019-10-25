de3998338fe9223e23bd90530bfd156a26a593ee
Humay <crypto347@gmail.com>
2019-10-25 17:29:27 +0400

    jest addDonkeySpace added

--- src/actions/businessActions.test.js
+++ src/actions/businessActions.test.js
@@ -690,4 +690,16 @@ describe('businessActions', () => {
         expect(store.getActions()).toEqual(expectedActions);
         expect(store.getActions()).toMatchSnapshot();
     })
+
+    it('Dispatches the correct action and payload (addDonkeySpace)', () => {
+        const expectedActions = [
+            {
+                type: actionTypes.ADD_DONKEY_SPACE
+            }
+        ];
+
+        store.dispatch(Actions.addDonkeySpace());
+        expect(store.getActions()).toEqual(expectedActions);
+        expect(store.getActions()).toMatchSnapshot();
+    })
   });
