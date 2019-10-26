659e730dba9b0b6b11da5f8638bd25bef3a78d16
Humay <crypto347@gmail.com>
2019-10-26 10:20:04 +0400

    jest improveMegaClippers added

--- src/actions/businessActions.test.js
+++ src/actions/businessActions.test.js
@@ -1428,5 +1428,18 @@ describe('businessActions', () => {
         expect(store.getActions()).toEqual(expectedActions);
         expect(store.getActions()).toMatchSnapshot();
     });
+
+    it('Dispatches the correct action and payload (improveMegaClippers)', () => {
+        const expectedActions = [
+            {
+                type: actionTypes.IMPROVE_MEGA_CLIPPERS,
+                val: 36
+            }
+        ];
+
+        store.dispatch(Actions.improveMegaClippers(36));
+        expect(store.getActions()).toEqual(expectedActions);
+        expect(store.getActions()).toMatchSnapshot();
+    });
     
 });
