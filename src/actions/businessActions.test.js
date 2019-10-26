267e6824933bc6aa6647cb062446ad67b1c51066
Humay <crypto347@gmail.com>
2019-10-26 09:52:07 +0400

    jest startInvestmentsDeposit added

--- src/actions/businessActions.test.js
+++ src/actions/businessActions.test.js
@@ -1112,4 +1112,16 @@ describe('businessActions', () => {
         expect(store.getActions()).toEqual(expectedActions);
         expect(store.getActions()).toMatchSnapshot();
     })
+
+    it('Dispatches the correct action and payload (startInvestmentsDeposit)', () => {
+        const expectedActions = [
+            {
+                type: actionTypes.START_INVESTMENTS_DEPOSIT
+            }
+        ];
+
+        store.dispatch(Actions.startInvestmentsDeposit());
+        expect(store.getActions()).toEqual(expectedActions);
+        expect(store.getActions()).toMatchSnapshot();
+    })
   });
