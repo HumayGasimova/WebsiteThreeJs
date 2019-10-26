81681f5c97db47fa473e2fb1e531dfe6d7a82d85
Humay <crypto347@gmail.com>
2019-10-26 10:13:20 +0400

    jest addChosenFromDropdown added

--- src/actions/businessActions.test.js
+++ src/actions/businessActions.test.js
@@ -1326,5 +1326,19 @@ describe('businessActions', () => {
         expect(store.getActions()).toEqual(expectedActions);
         expect(store.getActions()).toMatchSnapshot();
     });
+
+    it('Dispatches the correct action and payload (addChosenFromDropdown)', () => {
+        const expectedActions = [
+            {
+                type: actionTypes.ADD_CHOSEN_FROM_DROPDOWN,
+                chosen: "I am chosen",
+                index: 0
+            }
+        ];
+
+        store.dispatch(Actions.addChosenFromDropdown("I am chosen", 0));
+        expect(store.getActions()).toEqual(expectedActions);
+        expect(store.getActions()).toMatchSnapshot();
+    });
     
 });
