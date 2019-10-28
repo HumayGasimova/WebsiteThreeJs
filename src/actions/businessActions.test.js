6ae8add6c53de629b951de8bf67ea56b24db2ee8
Humay <crypto347@gmail.com>
2019-10-28 09:40:37 +0400

    jest startNewTournament added

--- src/actions/businessActions.test.js
+++ src/actions/businessActions.test.js
@@ -1454,5 +1454,17 @@ describe('businessActions', () => {
         expect(store.getActions()).toEqual(expectedActions);
         expect(store.getActions()).toMatchSnapshot();
     });
+
+    it('Dispatches the correct action and payload (startNewTournament)', () => {
+        const expectedActions = [
+            {
+                type: actionTypes.START_NEW_TOURNAMENT
+            }
+        ];
+
+        store.dispatch(Actions.startNewTournament(true));
+        expect(store.getActions()).toEqual(expectedActions);
+        expect(store.getActions()).toMatchSnapshot();
+    });
     
 });
