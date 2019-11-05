e45de20265d6eaab049077e2d5937fc883011d70
Humay <crypto347@gmail.com>
2019-11-05 12:06:37 +0400

    jest TOGGLE_THROWN_PROJECT_COMBINATORY_HARMONICS added

--- src/reducers/businessReducer.test.js
+++ src/reducers/businessReducer.test.js
@@ -2294,6 +2294,22 @@ describe('businessReducer', () => {
         }
         expect(reducer(initState, action)).toEqual(state);
     })
+
+    it("should update combinatoryHarmonicsIsThrown with the value passed through the action", () => {
+        const action = { 
+            type: actionTypes.TOGGLE_THROWN_PROJECT_COMBINATORY_HARMONICS,
+            val: true
+        }
+        const initState = {
+            ...initialState,
+            combinatoryHarmonicsIsThrown: false
+        }
+        const state = {
+            ...initialState,
+            combinatoryHarmonicsIsThrown: true
+        }
+        expect(reducer(initState, action)).toEqual(state);
+    })
     // it("should return the initial state", () => {
     //    expect(reducer(undefined, {})).toEqual(state);
     // })
