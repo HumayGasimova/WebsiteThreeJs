fd4b0f6bcfa6109d7d7ee21eff702bb7393b335b
Humay <crypto347@gmail.com>
2019-11-07 12:54:47 +0400

    jest showBusinessSection added

--- src/actions/businessActions.test.js
+++ src/actions/businessActions.test.js
@@ -2014,4 +2014,16 @@ describe('businessActions', () => {
         expect(store.getActions()).toMatchSnapshot();
     });
 
+    it('Dispatches the correct action and payload (showBusinessSection)', () => {
+        const expectedActions = [
+            {
+                type: actionTypes.SHOW_BUSINESS_SECTION,
+                val: true
+            }
+        ];
+
+        store.dispatch(Actions.showBusinessSection(true));
+        expect(store.getActions()).toEqual(expectedActions);
+        expect(store.getActions()).toMatchSnapshot();
+    });
 });
