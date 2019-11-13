6ff91eb2efcd5bfe9bc3a0be44e0be5dc630c9a8
Humay <crypto347@gmail.com>
2019-11-13 13:10:48 +0400

    New Project Canvas

---
+++ src/components/Canvas/canvas.js
@@ -0,0 +1,40 @@
+/**
+* Libraries
+*/
+
+import React,{
+    Component
+} from 'react';
+ 
+
+/**
+ * Components
+ */
+
+/**
+ * Styles
+ */
+
+import './canvas.scss';
+
+/**
+ * App component definition and export
+ */
+
+export class Canvas extends Component {
+
+/**
+* Markup
+*/
+
+    render(){
+        return(
+                <div className="app">
+                
+                </div>
+        );
+    }
+}
+
+export default Canvas;
+ 
