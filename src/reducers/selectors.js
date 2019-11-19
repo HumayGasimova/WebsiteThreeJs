e14fb3d42218bc17f9d3ecc2d09eb29047a909c0
Humay <crypto347@gmail.com>
2019-11-19 15:54:20 +0400

    code cleaning 2

--- src/reducers/selectors.js
+++ src/reducers/selectors.js
@@ -1,19 +1,3 @@
-import { createSelector } from 'reselect';
-
-const getCircles = (state) => state.canvas.circles;
-
-export const getCirclesState = createSelector(
-    [getCircles],
-    (x) => x
-);
-
-const getNumberOfBalls = (state) => state.canvas.numberOfBalls;
-
-export const getNumberOfBallsState = createSelector(
-    [getNumberOfBalls],
-    (x) => x
-);
-
 const getMousePressed = (state) => state.paint.mousePressed;
 
 export const getMousePressedState = createSelector(
