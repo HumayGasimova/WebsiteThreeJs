eae3c073610dc1cc4e2b6c40fd4ed23be5370987
Humay <crypto347@gmail.com>
2019-11-29 15:11:23 +0400

    toolbar text style added

--- src/components/Parts/Toolbar/toolbar.js
+++ src/components/Parts/Toolbar/toolbar.js
@@ -57,6 +57,11 @@ export const Toolbar = () => {
 
     return(
         <div className="toolbar" style={{width: `${size.width-100}px`}}>
+            <div>Home</div>
+            <div>About</div>
+            <div>Services</div>
+            <div>Gallery</div>
+            <div>Contact</div>
         </div>
     );
 }
