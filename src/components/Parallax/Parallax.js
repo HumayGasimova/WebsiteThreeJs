47e9d54a7ea8b3f989d537e2b8d98ab25a1847ef
Humay <crypto347@gmail.com>
2019-12-02 17:54:40 +0400

    refactoring

--- src/components/Parallax/Parallax.js
+++ src/components/Parallax/Parallax.js
@@ -1,76 +1,64 @@
 /**
- * Libraries
- */
+* Libraries
+*/
 
-import React,{
-   Component
-} from 'react';
+import React from 'react';
 
 /**
- * Components
- */
+* Components
+*/
+
 import HeaderText from './Text/HeaderText/headerText';
-import Stars from './Stars/stars';
 import Unicorn from './Unicorn/unicorn';
 import AirBalloon from './AirBalloon/airBalloon';
 import ImagesPage from './ImagePage/imagesPage';
 
 /**
- * Styles
- */
+* Styles
+*/
 
 import './parallax.scss';
 
-
-
 /**
- * App component definition and export
+ * Parallax component definition and export
  */
-export class Parallax extends Component {
+export const Parallax = () => {
 
    /**
-   * Constructor
+   * Mark up
    */
 
-   constructor(props) {
-      super(props);
-      
-   }
-
-   render(){
-      return(
-         <div className="main">
-            <div className="backgroundBox">
-               <div className="toolbar2"></div>
-               <HeaderText/>
-               <div className="parallax-airbaloons">
-                  <AirBalloon left/>
-                  <AirBalloon />
-               </div>
+   return(
+      <div className="parallax">
+         <div className="parallax-backgroundBox">
+            <HeaderText/>
+            <div className="parallax-airbaloons">
+               <AirBalloon left/>
+               <AirBalloon />
             </div>
+         </div>
 
+         <div className="text">
+            <h1>Text</h1>
+            <div>
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <br/>
+         </div>
+
+         <div className="parallax-backgroundBox2">
             <div className="text">
                <h1>Text</h1>
                <div>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                </div>
-               <br/>
+               <Unicorn left/>
+               <Unicorn />
             </div>
-
-            <div className="backgroundBox2">
-               <div className="text">
-                  <h1>Text</h1>
-                  <div>
-                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  </div>
-                  <Unicorn left/>
-                  <Unicorn />
-               </div>
-            </div>
-               <ImagesPage/>
          </div>
-      );
-   }
+            <ImagesPage/>
+      </div>
+   );
 }
 
 export default Parallax;
