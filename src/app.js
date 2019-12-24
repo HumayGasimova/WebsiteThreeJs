313c4a68d9aa1d96f5289862ef29d7a7f051dc8f
Humay <crypto347@gmail.com>
2019-12-24 12:49:49 +0400

    New Project

--- src/app.js
+++ src/app.js
@@ -14,13 +14,7 @@ import {
 * Components
 */
 
-import Home from './components/Home/home';
-import Service1 from './components/Parts/Services/Service1/service1';
-import Service2 from './components/Parts/Services/Service2/service2';
-import Service3 from './components/Parts/Services/Service3/service3';
-import Service4 from './components/Parts/Services/Service4/service4';
-import Service5 from './components/Parts/Services/Service5/service5';
-import Service6 from './components/Parts/Services/Service6/service6';
+import Tutorial from './components/Tutorial/tutorial';
 
 /**
 * Styles
@@ -44,41 +38,9 @@ export class App extends Component {
          <div className="app">
             <Route 
                exact
-               path="/service1"
-               component={ Service1 }
-            />
-             <Route 
-               exact 
-               path="/service2"
-               component={ Service2 }
-            />
-            <Route 
-               exact 
-               path="/service3"
-               component={ Service3 }
-            />
-            <Route 
-               exact 
-               path="/service4"
-               component={ Service4 }
-            />
-             <Route 
-               exact 
-               path="/service5"
-               component={ Service5 }
-            />
-            <Route 
-               exact 
-               path="/service6"
-               component={ Service6 }
-            />
-            <Route 
-               exact 
                path="/"
-               component={ Home }
+               component={ Tutorial }
             />
-            
-           
          </div>
       );
    }
---
+++ src/components/Tutorial/tutorial.js
@@ -0,0 +1,87 @@
+/**
+* Libraries
+*/
+
+import React, {
+    useEffect
+} from 'react';
+
+import {
+    connect
+} from 'react-redux';
+
+import {
+    bindActionCreators
+} from 'redux';
+
+/**
+* Components
+*/
+
+
+/**
+* Styles
+*/
+
+import './tutorial.scss';
+
+/**
+* Actions
+*/
+
+import * as Actions from '../../actions';
+
+/**
+* Selectors
+*/
+
+import * as Selectors from '../../reducers/selectors';
+
+/**
+* Constants
+*/
+
+import * as Icon from '../../constants/iconNames';
+
+/**
+* Home component definition and export
+*/
+
+export const Tutorial = (props) => {
+
+    /**
+    * Methods
+    */
+
+  
+
+    useEffect(() => {
+     
+    }, []);
+
+    /**
+    * Markup
+    */
+
+    return(
+        <div className="tutorial">
+            KKK
+        </div>
+    );
+}
+
+export default connect(
+    (state) => {
+        return {
+            // menuButtonIsPressed: Selectors.getMenuButtonIsPressedState(state),
+            // sidebarOnInit: Selectors.getSidebarOnInitState(state)
+        };
+    },
+    (dispatch) => {
+        return {
+            // menuButtonIsToggled: bindActionCreators(Actions.menuButtonIsToggled, dispatch),
+            // activateIcon: bindActionCreators(Actions.activateIcon, dispatch)
+        };
+    }
+)(Tutorial);
+ 
