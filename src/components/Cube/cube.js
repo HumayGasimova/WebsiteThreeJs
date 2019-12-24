593aef12a648876ad543cedd4363f78b6084b2e2
Humay <crypto347@gmail.com>
2019-12-24 17:50:25 +0400

    Light -> MeshPhongMaterial added

--- src/components/Cube/cube.js
+++ src/components/Cube/cube.js
@@ -80,11 +80,23 @@ export const Cube = (props) => {
         const boxDepth = 1;
 
         const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-        const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
+
+        // const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
+
+        // material which is affected by lights.
+        const material = new THREE.MeshPhongMaterial({color: 0x44aa88});
 
         const cube = new THREE.Mesh(geometry, material);
 
         scene.add(cube);
+
+        {
+            const color = 0xFFFFFF;
+            const intensity = 1;
+            const light = new THREE.DirectionalLight(color, intensity);
+            light.position.set(-1, 2, 4);
+            scene.add(light);
+        }
 
         // renderer.render(scene, camera);
 
@@ -93,6 +105,7 @@ export const Cube = (props) => {
            
             cube.rotation.x = time;
             cube.rotation.y = time;
+            cube.rotation.z = time;
            
             renderer.render(scene, camera);
            
