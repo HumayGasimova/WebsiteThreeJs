c8bdb86dbe3acea11980fb437210e85a613fe71e
Humay <crypto347@gmail.com>
2020-01-08 13:23:44 +0400

    media small projects updating in progress 1

--- src/components/SmallParts/Project/project.js
+++ src/components/SmallParts/Project/project.js
@@ -36,7 +36,7 @@ export const Project = (props) => {
     * State
     */
 
-    const [isHovering, setIsHovering] = useState(true);
+    const [isHovering, setIsHovering] = useState(false);
 
     /**
     * Methods
@@ -74,33 +74,59 @@ export const Project = (props) => {
     */
 
     return(
-        <div 
-            className={isHovering ? `${props.className}-hover` : props.className}
-        >
-            <a className="project-image" href={props.path}>
-                <div className={isHovering ? `${props.className}-info-open` : "project-info-close"}>
-                    <div className="project-info-name">{props.projectName}</div>
-                    <div className="project-info-description">{props.projectDescription}</div>
-                </div> 
-                <div className="project-image-border1">
-                    <div className="project-image-border2"   
-                        onMouseEnter={handleMouseEnter} 
-                        onMouseLeave={handleMouseLeave} 
-                    >
-                        <img src={loadImage(props.projectImage)}/>
+        <>
+            <div 
+                className={isHovering ? `${props.className}-hover` : props.className}
+            >
+                <a className="project-image" href={props.path}>
+                    <div className={isHovering ? `${props.className}-info-open` : "project-info-close"}>
+                        <div className="project-info-name">{props.projectName}</div>
+                        <div className="project-info-description">{props.projectDescription}</div>
+                    </div> 
+                    <div className="project-image-border1">
+                        <div className="project-image-border2"   
+                            onMouseEnter={handleMouseEnter} 
+                            onMouseLeave={handleMouseLeave} 
+                        >
+                            <img src={loadImage(props.projectImage)}/>
+                        </div>
                     </div>
-                </div>
-                <div className={isHovering ? `${props.className}-developer-info-open` : "project-developer-info-close"}>
-                    <div className="project-developer-info-photo">
-                        <img src={photo}/>
+                    <div className={isHovering ? `${props.className}-developer-info-open` : "project-developer-info-close"}>
+                        <div className="project-developer-info-photo">
+                            <img src={photo}/>
+                        </div>
+                        <div className="project-developer-info-general">
+                            <div className="project-developer-info-name">{props.developerName}</div>
+                            <div className="project-developer-info-position">{props.developerPosition}</div>
+                        </div>
                     </div>
-                    <div className="project-developer-info-general">
-                        <div className="project-developer-info-name">{props.developerName}</div>
-                        <div className="project-developer-info-position">{props.developerPosition}</div>
+                </a>
+            </div>
+            <div 
+                className="project-number-1-small"
+            >
+                <a className="project-image-small" href={props.path}>
+                    <div className={`${props.className}-info-open-small`}>
+                        <div className="project-info-name-small">{props.projectName}</div>
+                        <div className="project-info-description-small">{props.projectDescription}</div>
+                    </div> 
+                    <div className="project-image-border1-small">
+                        <div className="project-image-border2-small">
+                            <img src={loadImage(props.projectImage)}/>
+                        </div>
                     </div>
-                </div>
-            </a>
-        </div>
+                    <div className={`${props.className}-developer-info-open-small`}>
+                        <div className="project-developer-info-photo-small">
+                            <img src={photo}/>
+                        </div>
+                        <div className="project-developer-info-general-small">
+                            <div className="project-developer-info-name-small">{props.developerName}</div>
+                            <div className="project-developer-info-position-small">{props.developerPosition}</div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </>
     );
 }
 
