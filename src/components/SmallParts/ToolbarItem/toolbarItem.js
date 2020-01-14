2e8b3e5ac26351b735a887bd82b369469c647531
Humay <crypto347@gmail.com>
2020-01-14 12:52:29 +0400

    toolbar portfolio options added

--- src/components/SmallParts/ToolbarItem/toolbarItem.js
+++ src/components/SmallParts/ToolbarItem/toolbarItem.js
@@ -23,23 +23,44 @@ export const ToolbarItem = (props) => {
     * State
     */
 
+    const [isHovering, setIsHovering] = useState(false);
 
     /**
     * Methods
     */
 
+    const handleMouseEnter = (text) => {
+        if(text === "Portfolio"){
+            setIsHovering(true);
+        }
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovering(false);
+    }
 
     /**
     * Markup
     */
 
     return(
-        <div 
-            className={`${props.className}`}
-            onClick={props.onClick}
-        >
-            {props.text}
-        </div>
+        <>
+            <div 
+                className={`${props.className}`}
+                onClick={props.onClick}
+                onMouseEnter={() => handleMouseEnter(props.text)} 
+            >
+                {props.text}
+            </div>
+            {isHovering ? 
+                <div 
+                    className="toolbar-item-options" 
+                    onMouseLeave={handleMouseLeave} 
+                >
+                    <div className="toolbar-item-option">Portfolio</div>
+                    <div className="toolbar-item-option">Portfolio Single</div>
+                </div> : null}
+        </>
     );
 }
 
